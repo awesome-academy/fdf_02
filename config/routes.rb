fa1079cb1a99d162a5fe2ec840c429f1c6170471
VanNguyen21 <laivannguyen.97@gmail.com>
2019-03-07 08:00:53 +0700

    Create Home & SignUp Page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get  "/signup",  to: "users#new"
+  get  "/contact", to: "static_pages#contact"
+  root "static_pages#home"
+  resources :users
 end
